93c40a10ab407adafdf65b734016065ac3dd20ec
Deus-Ex-Umbra <gab.aparicio.ll@gmail.com>
2025-11-04 22:29:56 -0400

    general:fixes

--- frontend/src/servicios/api.ts
+++ frontend/src/servicios/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:3000';
+//const API_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = 'https://sicoprot-backend.ddns.net';
 
 export const api = axios.create({
   baseURL: `${API_URL}/api`,
